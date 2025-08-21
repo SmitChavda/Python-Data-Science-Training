79a1ab130eb01c1d30eba3870c22b9b45ade65e7
Smit Chavda <chavda.smit52@gmail.com>
2025-08-21 23:26:18 +0530

    Python Day 1

---
+++ Day 1/day1.py
@@ -0,0 +1,13 @@
+print("hello world!")
+# print("Hello "+input("What's your name?")+"!")
+
+name = "Jack"
+print(name)
+print(len(name))
+name = "Angela"
+print(name)
+print(len(name))
+
+username = input("What is your name?")
+nameLength = len(username)
+print(nameLength)
