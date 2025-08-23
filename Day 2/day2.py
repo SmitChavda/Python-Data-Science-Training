ebbfeaf63d7e3327b5c9092c4b717dc09de5fdde
Smit Chavda <chavda.smit52@gmail.com>
2025-08-24 00:06:51 +0530

    Day 2 - Data types - WIP

---
+++ Day 2/day2.py
@@ -0,0 +1,27 @@
+# subscripting
+print("mansi"[-1])
+print(["mansi", 3, 4, 5][2])
+
+
+# Integer = whole number
+print(123 + 456)
+
+# Large Integers
+print(123_456_789)
+
+# Float = Floating point number
+print(3.1845689)
+
+# Boolean
+print(True)
+print(False)
+
+# Type Error
+# len(123)
+print(len("12345"))
+
+# Type
+print(type(123))
+
+# Type casting
+print(int("345"))
---
+++ Day 2/task.py
@@ -0,0 +1,7 @@
+print(type(""))
+print(type(0))
+print(type(0.00))
+print(type(False))
+
+response = str(len(input("What's your name?")))
+print("Number of letters in your name: " + response)
